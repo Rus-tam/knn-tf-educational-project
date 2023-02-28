ff0125c1e6f582bf48c15d6ac227ccb2c8b33815
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-02-28 16:16:49 +0500

    applying standartization

--- index.js
+++ index.js
@@ -6,11 +6,41 @@ let { features, labels, testFeatures, testLabels } = loadCSV(
   {
     shuffle: true,
     splitTest: 10,
-    dataColumns: ["lat", "long"],
+    dataColumns: ["lat", "long", "sqft_lot"],
     labelColumns: ["price"],
   }
 );
 
-console.log(testFeatures);
-console.log(" ");
-console.log(testLabels);
+function knn(features, labels, predictionPoint, k) {
+  // Standard deviation
+  const { mean, variance } = tf.moments(features, 0);
+  const scaledPrediction = predictionPoint.sub(mean).div(variance.pow(0.5));
+  return (
+    features
+      .sub(mean)
+      .div(variance.pow(0.5))
+      .sub(scaledPrediction)
+      .pow(2)
+      .sum(1)
+      .pow(0.5)
+      .expandDims(1)
+      .concat(labels, 1)
+      .unstack()
+      .sort((a, b) => (a.arraySync()[0] > b.arraySync()[0] ? 1 : -1))
+      .slice(0, k)
+      .reduce((acc, pair) => acc + pair.arraySync()[1], 0) / k
+  );
+}
+
+features = tf.tensor(features);
+labels = tf.tensor(labels);
+// testFeatures = tf.tensor(testFeatures);
+// testLabels = tf.tensor(testLabels);
+
+testFeatures.forEach((testPoint, i) => {
+  const result = knn(features, labels, tf.tensor(testPoint), 10);
+  const err = ((testLabels[i][0] - result) / testLabels[i][0]) * 100;
+  console.log("Error", err);
+});
+
+// console.log("Guess", result, testLabels[0][0]);
